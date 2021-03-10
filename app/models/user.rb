d411c4b49ea15100311ab2bc1c5f7f3339180729
oramubon <cz.toshiki@gmail.com>
2021-03-10 20:22:23 +0900

    バリデーション再設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,17 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :email,            uniqueness: true
-  validates :nickname,         presence:   true
-  validates :family_name,      presence:   true
-  validates :first_name ,      presence:   true
-  validates :family_name_kana, presence:   true
-  validates :first_name_kana,  presence:   true
-  validates :birthday,         presence:   true
+  validates           :password, length: { minimum: 6 }
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
+  validates           :nickname, presence: true
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'Full-width characters' } do
+    validates         :family_name
+    validates         :first_name
+  end
+  with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: 'Full-width katakana characters' } do
+    validates         :family_name_kana
+    validates         :first_name_kana
+  end
+  validates           :birthday, presence: true
 end
