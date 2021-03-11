f5024b828584618973e489b417ce0059ba7cb88c
oramubon <cz.toshiki@gmail.com>
2021-03-11 22:50:28 +0900

    商品出品機能

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,10 +3,21 @@ class ProductsController < ApplicationController
   end
 
   def new
-    
+    @product = Product.new
   end
 
   def create
-    
+    @product = Product.create(product_params)
+    if @product.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:image, :product_name, :description, :category_id, :condition_id, :delivery_fee_id, :prefecture_id, :delivery_date_id, :price).merge(user_id: current_user.id)
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,27 @@
 class Product < ApplicationRecord
-  belongs_to  :user
+  belongs_to       :user
   has_one_attached :image
+
+  with_options presence: true do
+    validates :image
+    validates :product_name
+    validates :description
+    validates :price
+  end
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery_fee
+  belongs_to :prefecture
+  belongs_to :delivery_date
+
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :condition_id
+    validates :delivery_fee_id
+    validates :prefecture_id
+    validates :delivery_date_id
+  end
+
 end
