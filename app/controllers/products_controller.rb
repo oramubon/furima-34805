9052dc7ed4436acb0779469b38f20de6906c5a07
oramubon <cz.toshiki@gmail.com>
2021-03-12 20:41:44 +0900

    move_to_indexを定義

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,7 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
   before_action :set_product,        only: [:show, :edit, :update]
+  before_action :move_to_index,      only: [:edit, :update]
 
   def index
     @products = Product.order("created_at DESC")
@@ -23,9 +24,6 @@ class ProductsController < ApplicationController
   end
 
   def edit
-    unless current_user.id == @product.user_id
-      redirect_to action: :index
-    end
   end
 
   def update
@@ -45,5 +43,11 @@ class ProductsController < ApplicationController
 
   def set_product
     @product = Product.find(params[:id])
+  end
+
+  def move_to_index
+    unless current_user.id == @product.user_id
+      redirect_to action: :index
+    end
   end
 end
