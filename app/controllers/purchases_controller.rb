4910f91395d39a684eb8a3ccd956e63f6a9c811f
oramubon <cz.toshiki@gmail.com>
2021-03-15 22:13:24 +0900

    クレジット機能

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,8 +1,32 @@
 class PurchasesController < ApplicationController
   def index
     @product = Product.find(params[:product_id])
+    @purchase = Purchase.new
   end
 
   def create
+    @purchase = Purchase.create(purchase_params)
+    if @purchase.valid?
+      pay_product
+      @purchase.save
+      redirect_to root_path
+    else
+      render 'index'
+    end
+  end
+
+  private
+
+  def purchase_params
+    params.require(:purchase).merge(user_id: current_user.id, product_id: @product.id, token: params[:token])
+  end
+
+  def pay_product
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @product.price,
+      card: purchase_params[:token],
+      currency: 'jpy'
+    )
   end
 end
--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,2 +1,5 @@
 class Purchase < ApplicationRecord
+  belongs_to :product
+  attr_accessor :token
+  validates :token, presence: true
 end
