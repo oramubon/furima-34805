f1c68a9241e051e93841aaafca602acb9c0016aa
oramubon <cz.toshiki@gmail.com>
2021-03-17 16:25:53 +0900

    モデル単体テストコード

---
+++ spec/models/purchase_address_spec.rb
@@ -0,0 +1,74 @@
+require 'rails_helper'
+
+RSpec.describe PurchaseAddress, type: :model do
+  before do
+    user = FactoryBot.create(:user)
+    product = FactoryBot.create(:product)
+    @purchase_address = FactoryBot.build(:purchase_address, user_id: user.id, product_id: product.id)
+    sleep 0.1
+  end
+
+  describe '商品購入' do
+    context '商品購入できるとき' do
+      it 'すべての値が正しく入力されていれば保存できる' do
+        expect(@purchase_address).to be_valid
+      end
+      it 'buildingは空でも保存できる' do
+        @purchase_address.building = ''
+        expect(@purchase_address).to be_valid
+      end
+    end
+    context '商品購入できないとき' do
+      it "tokenが空では登録できない" do
+        @purchase_address.token = ''
+        @purchase_address.valid?
+        expect(@purchase_address.errors.full_messages).to include("Token can't be blank")
+      end
+      it 'post_codeが空では保存できない' do
+        @purchase_address.post_code = ''
+        @purchase_address.valid?
+        expect(@purchase_address.errors.full_messages).to include("Post code can't be blank")
+      end
+      it 'post_codeが半角のハイフンを含んだ正しい形式でないと保存できない' do
+        @purchase_address.post_code = '1234567'
+        @purchase_address.valid?
+        expect(@purchase_address.errors.full_messages).to include("Post code is invalid. Include hyphen(-)")
+      end
+      it 'prefectureを---以外を選択していないと保存できない' do
+        @purchase_address.prefecture = 1
+        @purchase_address.valid?
+        expect(@purchase_address.errors.full_messages).to include("Prefecture Select")
+      end
+      it 'cityが空では保存できない' do
+        @purchase_address.city = ''
+        @purchase_address.valid?
+        expect(@purchase_address.errors.full_messages).to include("City can't be blank")
+      end
+      it 'addressが空では保存できない' do
+        @purchase_address.address = ''
+        @purchase_address.valid?
+        expect(@purchase_address.errors.full_messages).to include("Address can't be blank")
+      end
+      it 'phone_numberが空では保存できない' do
+        @purchase_address.phone_number = ''
+        @purchase_address.valid?
+        expect(@purchase_address.errors.full_messages).to include("Phone number can't be blank")
+      end
+      it 'phone_numberが全角数字では保存できない' do
+        @purchase_address.phone_number = '０９０１２３４５６７８'
+        @purchase_address.valid?
+        expect(@purchase_address.errors.full_messages).to include("Phone number Input only number")
+      end
+      it 'phone_numberが12以上では保存できない' do
+        @purchase_address.phone_number = '090123456789'
+        @purchase_address.valid?
+        expect(@purchase_address.errors.full_messages).to include("Phone number Input only number")
+      end
+      it 'phone_numberが10以下では保存できない' do
+        @purchase_address.phone_number = '0901234567'
+        @purchase_address.valid?
+        expect(@purchase_address.errors.full_messages).to include("Phone number Input only number")
+      end
+    end
+  end
+end
