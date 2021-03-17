dbbb742800be8a8e450f18f4ad7b33a9261b1ca8
oramubon <cz.toshiki@gmail.com>
2021-03-17 19:11:16 +0900

    電話番号が１０桁でも登録できるように修正

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -9,7 +9,7 @@ class PurchaseAddress
     validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :city
     validates :address
-    validates :phone_number, format: {with: /\A[0-9]{11}\z/, message: "Input only number"}
+    validates :phone_number, format: {with: /\A[0-9]{10,11}\z/, message: "Input only number"}
   end
   validates :prefecture, numericality: {other_than: 1, message: "Select"}
 
